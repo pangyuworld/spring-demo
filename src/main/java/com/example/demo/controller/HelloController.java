c34ed19e7bd696d10419d381e4f8b14811811952
pangyuworld <a904237539@qq.com>
2020-11-25 15:18:49 +0800

    111

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Pang
  * @date 2020/11/25
